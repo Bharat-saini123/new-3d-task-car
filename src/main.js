16a002e24ac19e14ac36aafd00ad57be0ccefdc6
sainibharat277@gmail.com <sainibharat277@gmail.com>
2026-09-07 15:15:46 +0530

    update design

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ function cloneModel(index) { return models[index].clone(true); }
 function setStage(index, animate = false) {
   stage.clear();
   const model = cloneModel(index);
-  const displayScale = index >= 9 ? 0.5 : 1;
+  const displayScale = index >= 9 ? 0.5 : index >= 7 ? 0.8 : 1;
   if (animate) {
     const end = model.position.clone();
     model.position.y += 2.5; model.scale.setScalar(displayScale * 0.78); stage.add(model);
